58805ee678790211ff886a1f77c5096771c4916f
Łukasz Drożdż <lukaszd@fr24.com>
2021-07-29 23:46:24 +0200

    control block

--- tests.cpp
+++ tests.cpp
@@ -4,22 +4,35 @@
 
 namespace detail {
 
+template <typename T>
 struct ControlBlock {
-    ControlBlock() : m_count{1} {}
+public:
+    using ptr_t = T*;
+    using deleter_t = std::function<void(ptr_t)>;
+    explicit ControlBlock(deleter_t deleter)
+        : m_count{1}, m_deleter{std::move(deleter)} {}
     ControlBlock(const ControlBlock&) = delete;
 
     std::atomic<std::size_t> m_count;
+    void delete_owned(const ptr_t ptr) { m_deleter(ptr); }
+
+private:
+    const deleter_t m_deleter;
 };
 }  // namespace detail
 
 template <typename T>
 class SharedPtr {
 public:
+    using value_t = T;
     using ptr_t = T*;
     using reference_t = T&;
+    using deleter_t = typename detail::ControlBlock<T>::deleter_t;
 
-    explicit SharedPtr(ptr_t instance)
-        : m_control_block{new detail::ControlBlock{}}, m_ptr{instance} {}
+    explicit SharedPtr(ptr_t instance,
+                       deleter_t deleter = std::default_delete<value_t>{})
+        : m_control_block{new detail::ControlBlock{std::move(deleter)}},
+          m_ptr{instance} {}
 
     // TODO: handle class hierarchies
     // TODO: handle const T
@@ -28,19 +41,22 @@ public:
         ++m_control_block->m_count;
     }
 
-    //    explicit SharedPtr(SharedPtr&& other) noexcept
-    //        : m_control_block{other.m_control_block}, m_ptr{other.m_ptr}
-    //    {
-    //
-    //    }
+    explicit SharedPtr(SharedPtr&& other) noexcept
+        : m_control_block{other.m_control_block}, m_ptr{other.m_ptr} {
+        other.m_control_block = nullptr;
+        other.m_ptr = nullptr;
+    }
 
     ~SharedPtr() {
         // TODO: handle erased deleter
         // TODO: handle atomic access
-        --m_control_block->m_count;
-        if (!m_control_block->m_count.load()) {
-            delete m_ptr;
-            delete m_control_block;
+        if (m_control_block) {
+            --m_control_block->m_count;
+            if (!m_control_block->m_count) {
+                m_control_block->delete_owned(m_ptr);
+                // TODO: handle weak_ptr count
+                delete m_control_block;
+            }
         }
     }
 
@@ -48,7 +64,7 @@ public:
     reference_t operator*() noexcept { return *get(); }
 
 private:
-    detail::ControlBlock* m_control_block;
+    detail::ControlBlock<T>* m_control_block;
     ptr_t m_ptr;
 };
 
@@ -67,7 +83,7 @@ private:
  * NOTE: the requirements text tree of this file can be roughly displayed by:
     grep -oE \
          "(\s)*(AND_GIVEN|GIVEN|AND_WHEN|WHEN|AND_THEN|THEN|SECTION|TEST_CASE)\(\".*\"\)"\
-         tests/tests.cpp \
+         tests.cpp \
          | less
 */
 
@@ -90,9 +106,53 @@ TEST_CASE("Construction") {
     WHEN("copy-constructed") {
         SharedPtr<Traced> sut1{new Traced{}};
         SharedPtr<Traced> sut2{sut1};
-        THEN("holds the same pointer as original")
-        {
+        THEN("holds the same pointer as original") {
             CHECK(sut1.get() == sut2.get());
+        }
+    }
+    WHEN("move-constructed") {
+        SharedPtr<Traced> sut1{new Traced{}};
+        SharedPtr<Traced> sut2{std::move(sut1)};
+        THEN("the original is nullptr") {
+            CHECK(sut2.get());
+            CHECK(sut1.get() == nullptr);
+        }
+    }
+    WHEN("constructed with a pointer & deleter") {
+        bool deleter_called = false;
+        auto deleter = [&deleter_called](const Traced* ptr) {
+            delete ptr;
+            deleter_called = true;
+        };
+        THEN("destructs using the deleter") {
+            {
+                SharedPtr<Traced> sut{new Traced{}, deleter};
+                CHECK(Traced::alive_count() == 1);
+                CHECK(!deleter_called);
+            }
+            CHECK(Traced::alive_count() == 0);
+            CHECK(deleter_called);
+        }
+    }
+    GIVEN("a shared pointer shared between instances") {
+        REQUIRE(Traced::alive_count() == 0);
+        std::vector pointers = []() -> std::vector<SharedPtr<Traced>> {
+            SharedPtr<Traced> sut{new Traced{}};
+            return std::vector{SharedPtr<Traced>{sut}, SharedPtr<Traced>{sut},
+                               SharedPtr<Traced>{sut}};
+        }();
+        REQUIRE(Traced::alive_count() == 1);
+        WHEN("one of the pointers is destructed") {
+            pointers.pop_back();
+            THEN("the object is still valid") {
+                REQUIRE(Traced::alive_count() == 1);
+                AND_WHEN("all the shared pointers are destructed") {
+                    pointers.clear();
+                    THEN("the object is destroyed") {
+                        CHECK(Traced::alive_count() == 0);
+                    }
+                }
+            }
         }
     }
 }
